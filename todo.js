0d472864aee4dc764f7914011dc79d0fd3952689
humphreyokoth <humpasp@gmail.com>
2022-08-18 07:33:43 +0300

    Feat:appending items

--- todo.js
+++ todo.js
@@ -8,4 +8,20 @@
 
 window.onload=()=>{
   const form1 = document.querySelector("#addForm")
+let items = document.getElementById("items");
+let add = document.getElementById("btn-add");
+let editItem = null;
+form1.addEventListener("add",addItem);
+items.addEventListener("click",removeItem);
+
+}
+
+function addItem(e){
+  e.preventDefault();
+  if(add.value!= "Add"){
+  console.log("Please submit");
+  editItem.target.parentNode.childNodes[0].data= document.getElementById("input-add").value;
+  add.value = "Add";
+  document.getElementById("item").value = "";
+  }
 }
