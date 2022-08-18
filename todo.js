9fa654277b3ce3de62c7574580d7217991e634b2
humphreyokoth <humpasp@gmail.com>
2022-08-18 07:04:06 +0300

    Feat:Adding form

--- todo.js
+++ todo.js
@@ -1,3 +1,11 @@
-function name(params) {
-    
+// const myNodelist = document.getElementsByTagName("LI")
+
+// for(let i=0; i<myNodelist.length;i++){
+//   const span =document.createElement("SPAN");
+//   const txt = document.createTextNode("\u00D7")
+  
+// }
+
+window.onload=()=>{
+  const form1 = document.querySelector("#addForm")
 }
