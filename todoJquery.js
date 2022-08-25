69ce22e9472c5e28a3396f0e8025b8553da35ea4
humphreyokoth <humpasp@gmail.com>
2022-08-25 16:23:59 +0300

    Feat:Jquery

--- todoJquery.js
+++ todoJquery.js
@@ -1,8 +1,25 @@
+
+
+
+//  $(".blink-warning").fadeOut(1000,function(){
+//     $(this).fadeIn(1000);
+//     blinkWarning();
+// })
+
+
+
 $(document).ready(function(){
-    $(".submit").click(function(){
-        const inputVal = $(".custom-add-input").val();
+
+   
+    const inputVal = $(".custom-add").val();
        
-        $(".list-li").append("<li>"+inputVal+"</li>")
+    $(".addTask").on("submit",function(e){
+        e.preventDefault();
+       
+        if(inputVal.val()!=""){
+         $("<li>" + inputVal.val() +"</li>") .appendTo(".list-li")  
+        }
+        // $(".list-li").append("<li>" +inputVal+"</li>")
 
     })
 })
