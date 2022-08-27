d5ce76c6dcc00e80afdad72bd98ce71c30c2081a
humphreyokoth <humpasp@gmail.com>
2022-08-27 12:37:47 +0300

    Feat:Add and Remove

--- todoJquery.js
+++ todoJquery.js
@@ -1,24 +1,5 @@
 
 
-
-//  $(".blink-warning").fadeOut(1000,function(){
-//     $(this).fadeIn(1000);
-//     blinkWarning();
-// })
-
- 
-    // const inputVal = $(".custom-add").val();
-       
-    // $(".addTask").on("submit",function(e){
-    //     e.preventDefault();
-       
-    //     if(inputVal.val()!=""){
-    //      $("<li>" + inputVal.val() +"</li>") .appendTo(".list-li")  
-    //     }
-    //     // $(".list-li").append("<li>" +inputVal+"</li>")
-
-    // })
-
 $(document).ready(function(){
 $("#input-add").change(function(){
     const input = $(this).val();
